b774d8529c3895d73b0181b4b32d78b40370422f
Soin Roman <soinroma26@gmail.com>
2022-03-15 18:34:43 +0500

    add structure for counter js and add another pages

---
+++ js/counter/main.js
@@ -0,0 +1,12 @@
+$('.counter1').counterUp({
+    delay: 10,
+    time: 5000
+});
+$('.counter2').counterUp({
+    delay: 10,
+    time: 5000
+});
+$('.counter3').counterUp({
+    delay: 10,
+    time: 5000
+});
